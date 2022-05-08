d392880d6231b770f95332a701f6272b901a4390
Jackson Shen <shenj7@rose-hulman.edu>
2022-05-07 21:25:40 -0400

    fix

--- homeworks/15_advancedmem/forking_forth.c
+++ homeworks/15_advancedmem/forking_forth.c
@@ -22,204 +22,103 @@
 // file if you're curious
 void push_onto_forth_stack(struct forth_data *data, int64_t value_to_push);
 
+int ufd; // do i need this?
 
+int frames[NUM_PAGES*MAX_FORTHS]; //0 = unused, 1 = in use
+int page_table[NUM_PAGES];
+int forks[MAX_FORTHS];
 #define PAGE_UNCREATED -1
-
-int fd;
-int frames[NUM_PAGES*MAX_FORTHS]; // 1 and 0
-int forth_num_global;
-int forkslot[MAX_FORTHS];
+#define CREATED 1
 
 struct forth_extra_data {
     bool valid;
-    int page_table[NUM_PAGES]; // index = page num, value = frame#
     struct forth_data data;
+    int page_table[NUM_PAGES];
 };
 
+static void handler(int sig, siginfo_t *si, void *unused);
+
 struct forth_extra_data forth_extra_data[MAX_FORTHS];  
+
+int uforthnum = 0;
+
+int fork_forth();
+
+void switch_current_to(int forthnum);
 
 int used_pages_count;
 
 int get_used_pages_count() {
-    used_pages_count = 0;
+      used_pages_count = 0;
 
-    for(int i = 0; i < NUM_PAGES*MAX_FORTHS; i++){
+      for(int i = 0; i < NUM_PAGES*MAX_FORTHS; i++){
         if(frames[i] != PAGE_UNCREATED){
-            used_pages_count++;        }
-    }
-
+                      used_pages_count++;        
+        }
+            
+      }
     return used_pages_count;
 }
 
-/* Handler Section */
-static void handler(int sig, siginfo_t *si, void *unused)
-{
-    void* fault_address = si->si_addr;
-    int offset;
-
-    printf("in handler with invalid address %p\n", fault_address);
-    int distance = (void*) fault_address - (void*) UNIVERSAL_PAGE_START;
-    if(distance < 0 || distance > (getpagesize() * NUM_PAGES)) {
-        printf("address not within expected page!\n");
-        exit(2);
-    }
-
-    int page = distance / getpagesize();
-
-    offset = forth_extra_data[forth_num_global].page_table[page];
-    if(offset == PAGE_UNCREATED){
-        for(int i = 0; i < NUM_PAGES*MAX_FORTHS; i++){
-            if(frames[i] == PAGE_UNCREATED){
-                forth_extra_data[forth_num_global].page_table[page] = i;
-                frames[i] = 1;
-                offset = i;
-                break;
-            }
-        }
-    }
-    
-
-    printf("mapping page %d\n",page);
-    int address_dist = distance - (distance - (page * getpagesize())); 
-
-    char* result = mmap((void*) UNIVERSAL_PAGE_START + address_dist,
-                        getpagesize(),
-                        PROT_READ | PROT_WRITE | PROT_EXEC,
-                        MAP_FIXED | MAP_SHARED,
-                        fd, getpagesize()*offset);
-    if(result == MAP_FAILED) {
-        perror("map failed");
-        exit(1);
-    }
-    
-}
 
 bool first_time = true;
 void initialize_forths() {
     if(first_time) {
         // here's the place for code you only want to run once, like registering
         // our SEGV signal handler
-        static char stack[SIGSTKSZ];
-        stack_t ss = {
-                  .ss_size = SIGSTKSZ,
-                  .ss_sp = stack,
-        };
-    
-        sigaltstack(&ss, NULL);
-        struct sigaction sa;
-
-        sa.sa_flags = SA_SIGINFO | SA_ONSTACK;
-        sigemptyset(&sa.sa_mask);
-        sa.sa_sigaction = handler;
-
-        if (sigaction(SIGSEGV, &sa, NULL) == -1) {
-            perror("error installing handler");
-            exit(3);
-        }
-
-        fd = open("bigmem.dat", O_RDWR | O_CREAT | O_TRUNC, S_IRWXU);
-        if(fd < 0) {
-            perror("error loading linked file");
-            exit(25);
-        }   
-
-        char data = '\0';
-        lseek(fd, getpagesize()*NUM_PAGES*MAX_FORTHS - 1, SEEK_SET);
-        write(fd, &data, 1);
-        lseek(fd, 0, SEEK_SET);
-
-        // void* result = mmap(NULL,
-        //                 MAX_FORTHS*NUM_PAGES*getpagesize(),
-        //                 PROT_READ | PROT_WRITE | PROT_EXEC,
-        //                 MAP_SHARED,
-        //                 fd,
-        //                 0);
-        // if(result == MAP_FAILED) {
-        //     perror("map failed");
-        //     exit(1);
-        // }   
 
         first_time = false;
                
+        char filename[11];
+        sprintf(filename, "bigdat.dat");
+        int fd = open(filename, O_RDWR | O_CREAT, S_IRWXU);
+        if(fd < 0) {
+            perror("error loading linked file");
+            exit(25);
+        }
+        char data = '\0';
+        lseek(fd, getpagesize()*MAX_FORTHS*NUM_PAGES, SEEK_SET);
+        write(fd, &data, 1);
+        lseek(fd, 0, SEEK_SET);
+    //void* result = mmap(NULL,
+    //    MAX_FORTHS*NUM_PAGES*getpagesize(),
+    //    PROT_READ | PROT_WRITE | PROT_EXEC,
+    //    MAP_SHARED,
+    //    fd, 0);
+      static char stack[SIGSTKSZ];
+    
+      stack_t ss = {
+                  .ss_size = SIGSTKSZ,
+                  .ss_sp = stack,
+        };
+      sigaltstack(&ss, NULL);
+      struct sigaction sa;
+      sa.sa_flags = SA_SIGINFO | SA_ONSTACK;
+      sigemptyset(&sa.sa_mask);
+      sa.sa_sigaction = handler;
+      if (sigaction(SIGSEGV, &sa, NULL) == -1) {
+          perror("error installing handler");
+          exit(3);
+      }
+
+      ufd = fd;//do i need this?
     }
     // here's the place for code you want to run every time we run a test case
-    for(int i = 0; i < NUM_PAGES*MAX_FORTHS; i++){
-            frames[i] = PAGE_UNCREATED;
-        }
+    for (int i = 0; i < MAX_FORTHS*NUM_PAGES; i++) {
+      frames[i] = PAGE_UNCREATED;
+    }
 
     // mark all the forths as invalid
     for(int i = 0; i < MAX_FORTHS; i++) {
         forth_extra_data[i].valid = false;
-        forkslot[i] = PAGE_UNCREATED;
-        for(int j = 0; j < NUM_PAGES; j++){
-            forth_extra_data[i].page_table[j] = PAGE_UNCREATED;
+        forks[i] = PAGE_UNCREATED;
+        for (int j = 0; j < NUM_PAGES; j++) {
+        forth_extra_data[i].page_table[j] = PAGE_UNCREATED;
         }
     }
     
     used_pages_count = 0;
-}
 
-void switch_current_to(int forthnum){
-    forth_num_global = forthnum;
-    //do Universal unmapping
-    munmap((void*) UNIVERSAL_PAGE_START, MAX_FORTHS*NUM_PAGES*getpagesize());
-
-    for(int i = 0; i < NUM_PAGES; i++){
-        if(forth_extra_data[forthnum].page_table[i] != PAGE_UNCREATED){
-            char* result = mmap((void *) UNIVERSAL_PAGE_START + NUM_PAGES*i*getpagesize(),
-                            getpagesize(),
-                            PROT_READ | PROT_WRITE | PROT_EXEC,
-                            MAP_FIXED | MAP_SHARED,
-                            fd, forth_extra_data[forthnum].page_table[i]*getpagesize());
-            if(result == MAP_FAILED) {
-                perror("map failed");
-                exit(1);
-            }
-        }
-    }
-}
-
-int fork_forth(){
-    int valid_slot = -1;
-    
-    //Find open forth slot 
-    for(int i = 0; i < MAX_FORTHS; i++){
-        if (forkslot[i] != PAGE_UNCREATED){
-            valid_slot = i;
-            forkslot[i] = 1;
-            break;
-        }
-    }
-    
-    int pid = fork();
-    if(pid == 0){
-        // child
-        struct forth_data copiedData;
-        int child_table[NUM_PAGES];
-        memcpy(&copiedData, &forth_extra_data[forth_num_global].data, sizeof(struct forth_data));
-
-        // for(int i = 0; i < NUM_PAGES*MAX_FORTHS; i++){
-        //     if(frames[i] == PAGE_UNCREATED){
-
-        //         memcpy(&,forth_extra_data[forth_num_global].page_table[i])
-        //         break;
-        //     }
-        // }
-
-        for(int i = 0; i < NUM_PAGES*MAX_FORTHS; i++){
-            if(frames[i] == PAGE_UNCREATED){
-                forth_extra_data[forth_num_global].page_table[page] = i;
-                frames[i] = 1;
-                break;
-            }
-        }
-            
-        
-    }
-
-    int status;
-    wait(&status);
-    return pid;
 }
 
 int find_available_slot() {
@@ -239,27 +138,22 @@ int find_available_slot() {
 // This function creates a brand new forth instance (not a fork) with the given code
 // The function returns the id num of the newly created forth
 int create_forth(char* code) {
-    // printf("Forth created\n");
     int forth_num = find_available_slot();
     forth_extra_data[forth_num].valid = true;
+
 
     // STEP 0
     // this is where you should allocate NUM_PAGES*getpagesize() bytes
     // starting at position UNIVERSAL_PAGE_START to get started
     //
     // use mmap
-    // void* result = mmap((void*) UNIVERSAL_PAGE_START,
-    //                     NUM_PAGES*getpagesize(),
-    //                     PROT_READ | PROT_WRITE | PROT_EXEC,
-    //                     MAP_FIXED | MAP_SHARED | MAP_ANONYMOUS,
-    //                     -1,
-    //                     0);
-    // if(result == MAP_FAILED) {
-    //     perror("map failed");
-    //     exit(1);
-    // }    
-    switch_current_to(forth_num);
 
+    //void* result = mmap((void*) UNIVERSAL_PAGE_START,
+    //    NUM_PAGES*getpagesize(),
+    //    PROT_READ | PROT_WRITE | PROT_EXEC,
+    //    MAP_FIXED|MAP_SHARED|MAP_ANONYMOUS,
+    //    -1, 0);
+    switch_current_to(forth_num);
     // the return stack is a forth-specific data structure.  I
     // allocate a seperate space for it as the last 2 pages of
     // NUM_PAGES.
@@ -313,4 +207,92 @@ void push_onto_forth_stack(struct forth_data *data, int64_t value_to_push) {
                           // goes low
     *((int64_t*) data->stack_top) = current_top;
     
+}
+
+void switch_current_to(int forthnum){
+  uforthnum = forthnum;
+  forth_extra_data[forthnum].valid = true;
+  munmap((void*) UNIVERSAL_PAGE_START,
+      getpagesize()*NUM_PAGES*MAX_FORTHS);
+  //mmap((void*) UNIVERSAL_PAGE_START, 
+  //      getpagesize()*NUM_PAGES, 
+  //      PROT_READ | PROT_WRITE | PROT_EXEC,
+  //      MAP_FIXED | MAP_SHARED, 
+  //      ufd, 
+  //      forthnum*NUM_PAGES*getpagesize());
+  for (int i = 0; i < NUM_PAGES; i++) {
+    if (forth_extra_data[uforthnum].page_table[i] != PAGE_UNCREATED) {
+      mmap((void*)UNIVERSAL_PAGE_START + i*NUM_PAGES*getpagesize(),
+          getpagesize(),
+          PROT_READ | PROT_WRITE | PROT_EXEC,
+          MAP_FIXED | MAP_SHARED,
+          ufd,
+          forth_extra_data[forthnum].page_table[i]*getpagesize());
+    }
+    
+  }
+}
+
+
+static void handler(int sig, siginfo_t *si, void *unused)
+{
+    void* fault_address = si->si_addr;
+    int offset;
+    int distance = (void*) fault_address - (void*) UNIVERSAL_PAGE_START;
+    int page = distance/getpagesize();
+
+    printf("in handler with invalid address %p\n", fault_address);
+
+    offset = forth_extra_data[uforthnum].page_table[page];
+    
+    if (offset == PAGE_UNCREATED) {
+      for (int i = 0; i < NUM_PAGES*MAX_FORTHS; i++) {
+        printf("frame %d : %d\n", i, frames[i]);
+        if (frames[i] == PAGE_UNCREATED) {
+          forth_extra_data[uforthnum].page_table[page] = i;
+          frames[i] = CREATED;
+          offset = i;
+          break;
+        }
+      }
+    }
+
+    printf("mapping page %d\n", page);
+    if(distance < 0 || distance > (getpagesize() * NUM_PAGES)) {
+             printf("address not within expected page!\n");
+                          exit(2);
+                          
+    }
+
+    
+}
+
+int fork_forth() {
+  int loc = -1;
+
+  for (int i = 0; i < MAX_FORTHS; i++) {
+    if (forks[i] != PAGE_UNCREATED) {
+      loc = i;
+      forks[i] = CREATED;
+      break;
+    }
+  }
+
+  int pid = fork();
+  if (pid == 0) {
+    //child
+    struct forth_data dat;
+    int child_pages[NUM_PAGES];
+    memcpy(&dat, &forth_extra_data[uforthnum].data, sizeof(struct forth_data)); // copy forth data into child
+    for (int i = 0; i < NUM_PAGES*MAX_FORTHS; i++) {
+      if (frames[i] == PAGE_UNCREATED) {
+        forth_extra_data[forth_num_global].page_table[page] = i;
+        frames[i] = CREATED;
+        break;
+      }
+    }
+  }
+  int status;
+  wait(&status);
+  return pid;
 }
